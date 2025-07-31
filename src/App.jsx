8b60813ba55e77f5c10d9477b48d1fdb61fc59a3
Babashov Mahammad <babashov.info@gmail.com>
2025-07-30 23:39:23 -0400

    newTodo state added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [newTodo, setNewTodo] = useState('Exmaple Text');
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm />
+      <p>{newTodo}</p>
       <TodoList />
     </div>
   );
