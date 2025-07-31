b64013358cdb34dd7d83dd413a2696ce0e8f97a7
Babashov Mahammad <babashov.info@gmail.com>
2025-07-30 23:39:50 -0400

    TodoListItem.jsx file added and used it

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,9 +1,16 @@
+import TodoListItem from './TodoListItem';
 function TodoList() {
   const todos = [
     { id: 1, title: 'review resources' },
     { id: 2, title: 'take notes' },
     { id: 3, title: 'code out app' },
   ];
-  return todos.map((todo) => <li key={todo.id}>{todo.title}</li>);
+  return (
+    <ul>
+      {todos.map((todo) => {
+        return <TodoListItem key={todo.id} todo={todo} />;
+      })}
+    </ul>
+  );
 }
 export default TodoList;
