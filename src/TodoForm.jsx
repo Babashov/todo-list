3c198e6db779361ed3794dfd3da901443034a521
Babashov Mahammad <babashov.info@gmail.com>
2025-08-06 18:38:26 -0400

    useRef hook used for focus on the input, get props from parent component, added handleAddTodo event

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,10 +1,22 @@
-function TodoForm() {
+import { useRef } from 'react';
+
+function TodoForm({ onAddTodo }) {
+  const todoTitleInput = useRef('');
+
+  function handleAddTodo(event) {
+    event.preventDefault();
+    const title = event.target.title.value;
+    onAddTodo(title);
+    event.target.title.value = '';
+    todoTitleInput.current.focus();
+  }
+
   return (
-    <>
+    <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle">Todo</label>
-      <input type="text" id="todoTitle" />
+      <input ref={todoTitleInput} name="title" type="text" id="todoTitle" />
       <button>Add Todo</button>
-    </>
+    </form>
   );
 }
 export default TodoForm;
