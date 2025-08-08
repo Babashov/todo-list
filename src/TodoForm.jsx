7030ee6c94ec04d02f0ec1c7ac0b86a740c7d36b
Babashov Mahammad <babashov.info@gmail.com>
2025-08-08 10:57:45 -0400

    Added Controlled input:todoTitle

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,4 +1,16 @@
-function TodoForm() {
+import { useRef } from 'react';
+
+function TodoForm({ onAddTodo }) {
+  const todoTitleInput = useRef('');
+
+  function handleAddTodo(event) {
+    event.preventDefault();
+    const title = event.target.title.value;
+    onAddTodo(title);
+    event.target.title.value = '';
+    todoTitleInput.current.focus();
+  }
+
   return (
     <>
       <label htmlFor="todoTitle">Todo</label>
