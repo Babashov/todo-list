604b2b94e370baffff0f3c4680ab2c29f5978791
Babashov Mahammad <babashov.info@gmail.com>
2025-08-08 10:41:12 -0400

    completeTodo method is created, isCompleted added to newTodo, completeTodo passed as a prop to child component

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,20 @@ import { useState } from 'react';
 function App() {
   const [todoList, setTodoList] = useState([]);
   const [isTodolistHave, setIsTodlistHave] = useState(false);
+
   function addTodo(title) {
-    const newTodo = { title, id: Date.now() };
+    const newTodo = { title, id: Date.now(), isCompleted: false };
     setTodoList([...todoList, newTodo]);
+  }
+
+  function completeTodo(id) {
+    const updatedTodos = todoList.map((todo) => {
+      if (todo.id === id) {
+        return { ...todoList, isCompleted: true };
+      }
+      return todo;
+    });
+    setTodoList(updatedTodos);
   }
 
   function addisTodolistHave(isTodolistHave) {
@@ -20,7 +31,7 @@ function App() {
       <TodoForm onAddTodo={addTodo} addisTodolistHave={addisTodolistHave} />
 
       {isTodolistHave ? (
-        <TodoList todoList={todoList} />
+        <TodoList todoList={todoList} onCompleteTodo={completeTodo} />
       ) : (
         <p>Add todo above to get started</p>
       )}
