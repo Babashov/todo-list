6c67b0e8796cee0d71d4b830c19ab519716feded
Babashov Mahammad <babashov.info@gmail.com>
2025-08-13 00:13:17 -0400

    Allow Users to Edit Existing Todos

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -1,9 +1,51 @@
-function TodoListItem({ todo, onCompleteTodo }) {
+import { useState } from 'react';
+import TextInputWithLabel from '../../shared/TextInputWithLabel';
+function TodoListItem({ todo, onCompleteTodo, onUpdateTodo }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [workingTitle, setWorkingTitle] = useState(todo.title);
+
+  function handleCancel(e) {
+    e.preventDefault();
+    setWorkingTitle(todo.title);
+    setIsEditing(false);
+  }
+
+  function handleEdit(e) {
+    e.preventDefault();
+    setWorkingTitle(e.target.value);
+  }
+
+  function handleUpdate(e) {
+    if (!isEditing) return;
+    e.preventDefault();
+    onUpdateTodo({ ...todo, title: workingTitle });
+    setIsEditing(false);
+  }
+
   return (
-    <form onChange={() => onCompleteTodo(todo.id)}>
-      <input type="checkbox" />
-      <li>{todo.title}</li>
-    </form>
+    <li>
+      <form onSubmit={handleUpdate}>
+        {isEditing ? (
+          <>
+            <TextInputWithLabel onChange={handleEdit} value={workingTitle} />
+            <button onClick={handleCancel}>Cancel</button>
+            <button onClick={handleUpdate}>Update</button>
+          </>
+        ) : (
+          <>
+            <label>
+              <input
+                type="checkbox"
+                id={`checkbox${todo.id}`}
+                checked={todo.isCompleted}
+                onChange={() => onCompleteTodo(todo.id)}
+              />
+            </label>
+            <span onClick={() => setIsEditing(true)}>{todo.title}</span>
+          </>
+        )}
+      </form>
+    </li>
   );
 }
 
