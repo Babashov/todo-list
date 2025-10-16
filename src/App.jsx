18a6b64365b6a68090d0c4a703082db6be1528ca
Babashov Mahammad <babashov.info@gmail.com>
2025-10-16 02:56:34 -0400

    useState removed from import

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import TodoList from './features/TodoList/TodoList';
 import TodoForm from './features/TodoForm';
 import TodosViewForm from './features/TodosViewForm';
 import './App.css';
-import { useState, useReducer, useCallback, useEffect } from 'react';
+import { useReducer, useCallback, useEffect } from 'react';
 
 import {
   reducer as todosReducer,
